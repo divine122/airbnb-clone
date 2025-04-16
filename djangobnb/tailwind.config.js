1927413cf9d1ba153e2c0e177f6b5577eda5ea11
divine122 <ogochukwudivine221@gmail.com>
2025-04-16 14:22:51 +0100

    install and setup Nextjs, create project

---
+++ djangobnb/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'airbnb': '#ff385c',
+        'airbnb-dark': '#d50027'
+      }
+    },
+  },
+  plugins: [],
+};
