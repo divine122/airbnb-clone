51e4801003301ce3926b68c17070b34996d72bd0
divine122 <ogochukwudivine221@gmail.com>
2025-04-30 18:52:30 +0100

    Code for nav bar done

--- djangobnb/app/page.tsx
+++ djangobnb/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-      DjangoBnb
+      Django Bnb
 
       <h2 className="text-airbnb">Django and next rules</h2>
     </main>
--- djangobnb/tailwind.config.js
+++ djangobnb/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {
@@ -14,4 +14,5 @@ module.exports = {
     },
   },
   plugins: [],
-};
+}
+
