34baf302270973f41f1b9b9186408ecaece1e0ef
divine122 <ogochukwudivine221@gmail.com>
2025-05-01 21:31:57 +0100

    List properties on the front page(static) done

---
+++ djangobnb/app/components/properties/PropertyList.tsx
@@ -0,0 +1,12 @@
+import PropertyListItem from "./propertyListItem";
+const PropertyList = () => {
+    return (
+        <>
+          <PropertyListItem />
+          <PropertyListItem />
+          <PropertyListItem />
+        </>
+    )
+}
+
+export default PropertyList;
