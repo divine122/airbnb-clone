3ca4f4e7ff521406b047380e3a40200af35200a6
divine122 <ogochukwudivine221@gmail.com>
2025-05-08 14:33:59 +0100

    Inbox page(static)done

---
+++ djangobnb/app/components/inbox/Conversation.tsx
@@ -0,0 +1,11 @@
+const Conversation = () => {
+    return (
+        <div className="px-6 py-4 border border-gray-300 rounded-xl">
+            <p className="mb-6 text-xl">Dee</p>
+
+            <p className="text-airbnb-dark cursor-pointer">Go to conversation</p>
+        </div>
+    )
+}
+
+export default Conversation;
