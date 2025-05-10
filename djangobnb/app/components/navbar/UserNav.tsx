aac9740720e74ee02cd42a5d4e4c367f7ae63c51
divine122 <ogochukwudivine221@gmail.com>
2025-05-10 16:35:44 +0100

    User menu popup done

--- djangobnb/app/components/navbar/UserNav.tsx
+++ djangobnb/app/components/navbar/UserNav.tsx
@@ -1,4 +1,11 @@
+'use client';
+
+import { useState } from "react";
+import MenuLink from "./MenuLink";
 const UserNav = () => {
+    const[isOpen, setisOpen] = useState(true)
+
+
     return (
         <div className="p-2 relative inline-block border rounded=full">
             <button className="flex items-center">
@@ -12,6 +19,21 @@ const UserNav = () => {
 
 
             </button>
+
+            {isOpen && (
+                <div className="w-[220px] absolute top-[60px] right-0 bg-white border rounded-xl shadow-md flex flex-col cursor-pointer">
+                    <MenuLink 
+                        label="Log in"
+                        onClick={() => console.log('Clicked button')}
+                    />
+                    
+
+                    <MenuLink 
+                        label="Sign up"
+                        onClick={() => console.log('Clicked button')}
+                    />
+                </div>
+            )}
         </div>
     )
 }
