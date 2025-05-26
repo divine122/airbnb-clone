bf4d1d1aa8deb10ec13c692e767286242e60010e
divine122 <ogochukwudivine221@gmail.com>
2025-05-26 15:26:16 +0100

    Get properties in the frontend and list them done

--- djangobnb/app/components/properties/PropertyList.tsx
+++ djangobnb/app/components/properties/PropertyList.tsx
@@ -1,10 +1,48 @@
+'use client';
+
+import { useEffect,useState } from "react";
 import PropertyListItem from "./propertyListItem";
+import { error } from "console";
+
+export type PropertyType = {
+   id:string;
+   title:string;
+   image_url:string;
+   price_per_night:number;
+}
+
 const PropertyList = () => {
+   const [properties, setproperties] = useState<PropertyType[]>([]);
+
+   const getproperties = async () => {
+      const url = 'http://localhost:8000/api/properties/';
+
+      await fetch(url, {
+        method :"GET"
+      })
+        .then(response => response.json())
+        .then((json) => {
+          console.log('json',json);
+
+          setproperties(json.data)
+        })
+        .catch((error) => {
+          console.log('error',error);
+        })
+    };
+  useEffect(() => {
+    getproperties();
+  }, []);
     return (
         <>
-          <PropertyListItem />
-          <PropertyListItem />
-          <PropertyListItem />
+          {properties.map((property) => {
+            return (
+              <PropertyListItem 
+                key={property.id}
+                property={property}
+              />
+            )
+          })}
         </>
     )
 }
--- djangobnb/backend/djangobnb_backend/core/settings.py
+++ djangobnb/backend/djangobnb_backend/core/settings.py
@@ -51,7 +51,7 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:8000',
+    'http://localhost:3000',
     'http://127.0.0.1:3000',
 ]
 
